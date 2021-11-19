4b15ccb573dcdcb96783ab767b4b4c0c26c770f1
Roderic Russell <rrr5458@gmail.com>
2021-11-19 01:52:54 -0500

    layout changes

--- travel.js
+++ travel.js
@@ -8,7 +8,7 @@ require([
     "esri/Graphic",
     "esri/layers/GraphicsLayer",
 
-//All code is contained in this function in order to use the modules as paramters
+    //All code is contained in this function in order to use the modules as paramters
 ], function (esriConfig, Map, SceneView, PopupTemplate, Graphic, GraphicsLayer) {
 
     esriConfig.apiKey = "AAPK2ecbffa6b04c4595986f15d7b92b786360Vh3K-cqrwl9Lkxt35QsY601Zr-1ZKleqAXXTL-KMOrsqWOKsTFOz-P2vf8-Bpc";
@@ -34,7 +34,7 @@ require([
             latitude: latitude
         };
         //properties of orange markers
-        const orangeMarkerSymbol = { 
+        const orangeMarkerSymbol = {
             type: "simple-marker",
             color: [226, 119, 40],  // Orange
             size: mag,
@@ -42,7 +42,7 @@ require([
                 color: [255, 255, 255], // White
                 width: 1
             }
-        
+
         };
         //properties of blue markers
         const blueMarkerSymbol = {
@@ -74,23 +74,22 @@ require([
                 width: 1
             }
         };
-        //????????
         let pointGraphic;
         //changes color and size of point based on mag
-        if(mag < 7) { //turns marker orange
+        if (mag < 7) { //turns marker orange
 
             pointGraphic = new Graphic({
                 geometry: point,
                 symbol: orangeMarkerSymbol,
-                attributes: properties, 
+                attributes: properties,
                 popupTemplate: { // earthquake details with point clicked
                     title: "{title}",
-                    content: 
-                    `Magnitude: {mag} <br>
+                    content:
+                        `Magnitude: {mag} <br>
                     <br>
                     ${convertedCoordinates}
                     `,
-                    
+
                 }
             });
 
@@ -106,7 +105,7 @@ require([
                     <br>
                     ${convertedCoordinates}
                     `,
-                    
+
                 }
             });
         } else if (mag < 34.999 && mag >= 17.495) { //turns marker green
@@ -121,7 +120,7 @@ require([
                     <br>
                     ${convertedCoordinates}
                     `,
-                    
+
                 }
             });
         } else if (mag >= 34.999) { //turns marker red
@@ -132,8 +131,8 @@ require([
                 attributes: properties,
                 popupTemplate: {
                     title: "{title}",
-                    content: 
-                    `Magnitude: {mag} <br>
+                    content:
+                        `Magnitude: {mag} <br>
                     <br>
                     ${convertedCoordinates}
                     `,
@@ -144,11 +143,11 @@ require([
         graphicsLayer.add(pointGraphic);
         //builds points object above when addPoints() is called. This will be used in moveView() to remove highlights
         points[id] = {
-            graphic : pointGraphic,
-            data : {longitude, latitude, properties, mag}
+            graphic: pointGraphic,
+            data: { longitude, latitude, properties, mag }
         }
     }
-     // starting container/frame for the map
+    // starting container/frame for the map
     let view = new SceneView({
         container: "viewDiv",
         map: map,
@@ -177,33 +176,31 @@ require([
         });
         //highlights the selected earthquake
         points[id].graphic.symbol.outline.width = 4
-        points[id].graphic.symbol.outline.color = [64,224,208]
+        points[id].graphic.symbol.outline.color = [64, 224, 208]
         points[id].graphic.symbol.size = mag * 7
+    }
+    //removes highlight on second click
+    function removeHighlight() {
+        //returns an array of a given object's own property names; keys here = the ids of the earthquakes
+        const keys = Object.keys(points)
+        for (let index = 0; index < keys.length; index++) {
+            //outline width was 4 and turqoise, in order to remove it, we set it to 1 and white, like it is orginally after addPoints()
+            points[keys[index]].graphic.symbol.outline.width = 1
+            points[keys[index]].graphic.symbol.outline.color = [255, 255, 255]
+            points[keys[index]].graphic.symbol.size = points[keys[index]].data.mag
+
+
         }
-        //removes highlight on second click
-        function removeHighlight () {
-            //returns an array of a given object's own property names; keys here = the ids of the earthquakes
-            const keys = Object.keys(points)
-            console.log(keys)
-            for (let index = 0; index < keys.length; index++) {
-                //outline width was 4 and turqoise, in order to remove it, we set it to 1 and white, like it is orginally after addPoints()
-                points[keys[index]].graphic.symbol.outline.width = 1
-                points[keys[index]].graphic.symbol.outline.color = [255, 255, 255]
-                points[keys[index]].graphic.symbol.size = points[keys[index]].data.mag
-                
-    
-}
-        }
+    }
 
 
     function renderCard(data) {
         return `<div class="card-base col">
-    <div class="card mb-2">
-        <img class="card-img-top" src="./TriangleEQ.png" alt="Card image cap">
+    <div class="card">
         <div class="card-body">
-            <h4 class="card-title">${data.properties.title}</h4>
+            <h6 class="card-title">${data.properties.title}</h6>
             <div class="earthquake-info">
-                <h6 class="card-text">Time: ${new Date(data.properties.time)}</h6>
+                <p class="card-text">${new Date(data.properties.time)}</p>
                 <p class="card-text">Magnitude: ${Number(data.properties.mag).toFixed(2)}<br>
                 Depth: ${Number(data.geometry.coordinates[2]).toFixed(2)} km</p>
 
@@ -216,35 +213,19 @@ require([
     }
 
     // Renders a slide with 3 cards as arguments (line 259)
-    function renderSlide(card1, card2, card3) {
+    function renderSlide(info) {
         // Each individual slide's Html layout includes the string portion below.
         let slideHtml = `
 <div class="carousel-item">
     <div class="container container-fluid">
         <div class="row card-row">`
-        // When data is passed into the 'card1' parameter, it is rendered in the 
-        // first position of the 'card-row'
-        if (card1) {
-            slideHtml += renderCard(card1)
-        }
-        // When data is passed into the 'card2' parameter, it is rendered in the 
-        // second position of the 'card-row'
-        if (card2) {
-            slideHtml += renderCard(card2)
-        }
-        // When data is passed into the 'card3' parameter, it is rendered in the 
-        // third position of the 'card-row'
-        if (card3) {
-            slideHtml += renderCard(card3)
-        }
-        // The ending/closing html is added (+=) to the string and then returned to be 
-        // displayed as the carousel innerHTML(line 259)
+            slideHtml += renderCard(info)
         slideHtml += `
         </div>
     </div>
 </div>`
-    return slideHtml
-}
+        return slideHtml
+    }
 
     // erathquake API fetch to get data
     fetch('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-07-07&limit=60')
@@ -255,30 +236,28 @@ require([
 
             let carousel = document.querySelector('.carousel-inner')
             carousel.innerHTML = ''
-            for (let index = 0; index < 60; index += 3) {
-                carousel.innerHTML += renderSlide(data.features[index], data.features[index + 1], data.features[index + 2])
+            for (let index = 0; index < 60; index++) {
+                carousel.innerHTML += renderCard(data.features[index])
             }
-            document.querySelector('.carousel-item').classList.add('active')
             //calls addPoint() for every earthquake
             for (let index = 0; index < data.features.length; index++) {
                 addPoint(data.features[index].geometry.coordinates[0], data.features[index].geometry.coordinates[1], data.features[index].properties, data.features[index].properties.mag * 7, data.features[index].id)
             }
 
         })
-        //triggers removeHighlight() and moveView() when user cliks "See More". 
+    //triggers removeHighlight() and moveView() when user cliks "See More". 
     document.addEventListener('click', function (e) {
         if (e.target.classList.contains('btn-primary')) {
+            console.log("hello")
             const longitude = e.target.dataset.longitude;
             const latitude = e.target.dataset.latitude;
-            const title = e.target.dataset.title;
-            const time = e.target.dataset.time;
             const mag = e.target.dataset.mag
             const id = e.target.dataset.id
 
             removeHighlight()
-            moveView(longitude , latitude, mag, id)
-        
-        
+            moveView(longitude, latitude, mag, id)
+
+
         }
     });
 
